da355d893f72192baa1ce35aafa394274ec32c9c
yawwwn <thngyaowen@gmail.com>
2023-07-30 22:12:16 +0800

    test

---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    // Set your Google Autocomplete API key here
+    const API_KEY = 'AIzaSyAty9Uem6U_-mD_q3Rjk0KfewHT5yrGan8';
+    const BASE_URL = 'https://maps.googleapis.com/maps/api/place/autocomplete/json';
+
+    // Get references to the input field and suggestions list
+    const searchInput = $('#searchInput');
+    const suggestionsList = $('#suggestions');
+
+    // Add an event listener to the input field to detect changes in the text
+    searchInput.on('input', function() {
+        const query = searchInput.val();
+
+        // Clear the existing suggestions
+        suggestionsList.empty();
+
+        // Make an API request to Google Autocomplete API
+        $.ajax({
+            url: BASE_URL,
+            dataType: 'json',
+            data: {
+                input: query,
+                key: API_KEY
+            },
+            success: function(data) {
+                const predictions = data.predictions;
+                if (predictions) {
+                    predictions.forEach(function(prediction) {
+                        suggestionsList.append('<li>' + prediction.description + '</li>');
+                    });
+                }
+            }
+        });
+    });
+});
